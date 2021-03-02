feb36b0443d4edc8f8d5e6f8a00ebd97008f908c
andyb <acbeers@pm.me>
2021-03-01 19:22:56 -0500

    Update BusStation.sol
    added timelock functionality, passed in days in the constructor, renamed and organized variables. added modifiers, upgraded solidity version

--- contracts/BusStation.sol
+++ contracts/BusStation.sol
@@ -1,15 +1,22 @@
-pragma solidity 0.5.17;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.0;
 
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
-contract BusStation {
+contract BusStation is Ownable {
     
     /* ==== Variables ===== */
     
     mapping(address => uint256) public _seats;
-    bool _bus_has_left;
-    uint256 _ticketTotal;
-    uint256 _min_value;
+    bool public _hasBusLeft;
+    uint256 public _ticketTotal;
+    uint256 _minTicketValue = 0;
+    uint256 _minAmountToLeave;
     address payable _destination;
+    
+    uint256 private constant _daysMultiplier = 60 * 60 * 24; // seconds in a day
+    uint256 public _timelockDuration;
+    uint256 public _endOfTimelock;
     
     /* ==== Events ===== */
     event TicketPurchased(address indexed _from, uint _value);
@@ -18,37 +25,51 @@ contract BusStation {
     /* ==== Constructor ===== */
     constructor(
         address payable destination,
-        uint256 min_value    
-    ) public {
-        _bus_has_left = false;
-        _min_value = min_value;
+        uint256 minAmountToLeave,
+        uint256 timelockDays
+    ) {
+        super;
+        _hasBusLeft = false;
+        _minAmountToLeave = minAmountToLeave;
         _destination = destination;
+        _timelockDuration = timelockDays * _daysMultiplier;
+        _endOfTimelock = block.timestamp + _timelockDuration;
     }
 
-    
-    /* ==== Views ===== */
-   function ticketTotal() public view returns(uint256) {
-       return _ticketTotal;
-   }
-   
-   function hasBusLeft() public view returns(bool) {
-       return _bus_has_left;
-   }
-
     /* ==== Functions ===== */
-   function buyBusTicket() public payable {
-       require(_bus_has_left == false);
-       _seats[msg.sender] += msg.value;
-       _ticketTotal += msg.value;
-       emit TicketPurchased(msg.sender, msg.value);
+    function buyBusTicket() public payable canPurchaseTicket{
+        _seats[msg.sender] += msg.value;
+        _ticketTotal += msg.value;
+        emit TicketPurchased(msg.sender, msg.value);
     }  
 
-   function triggerBusRide() external {
-        require(_ticketTotal > _min_value);
+   function triggerBusRide() external isReadyToRide{
         _destination.transfer(_ticketTotal); 
-        _bus_has_left = true;
+        _hasBusLeft = true;
         emit BusDeparts(_ticketTotal);
-        
    }
+   
+   /* === Modifiers === */
+   
+    modifier canPurchaseTicket() {
+        require(_hasBusLeft == false, "The bus already left!");
+        require(msg.value > _minTicketValue, "Need to pay something for the ticket.");
+        _;
+    }
     
+    modifier isReadyToRide() {
+        require(_endOfTimelock <= block.timestamp, "Function is timelocked");
+        require(_ticketTotal >= _minAmountToLeave, "Not enough money to leave.");
+        _;
+    }
+
+   /* === Locking ==== */
+
+    function lockFunction() external onlyOwner {
+        _endOfTimelock = block.timestamp + _timelockDuration;
+    }
+  
+    function unlockFunction() external onlyOwner {
+        _endOfTimelock = 0;
+    }
 }
