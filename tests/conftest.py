e61fdbed192e6bd503f92089a18b899d459d72ba
Gerrit <gerrit@rezscore.com>
2021-03-03 11:03:21 -0800

    Additional tests and formatting

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,20 +2,60 @@
 
 import pytest
 
+
 @pytest.fixture(scope="function", autouse=True)
 def isolate(fn_isolation):
     # perform a chain rewind after completing each test, to ensure proper isolation
     # https://eth-brownie.readthedocs.io/en/stable/tests-pytest-intro.html#isolation-fixtures
     pass
 
+
 @pytest.fixture(scope="module")
 def lockedBusStation(BusStation, accounts):
-    etherInWei = 10**18
-    return BusStation.deploy(accounts[0], etherInWei, 4, {'from': accounts[1]})
+    etherInWei = 10 ** 18
+    return BusStation.deploy(
+        accounts[0], etherInWei, 4, 10 ** 20, {"from": accounts[1]}
+    )
+
 
 @pytest.fixture(scope="module")
 def unlockedBusStation(BusStation, accounts, chain):
-    etherInWei = 10**18
+    etherInWei = 10 ** 18
     sleep_time = 5 * 60 * 60 * 24
     chain.sleep(sleep_time)
-    return BusStation.deploy(accounts[0], etherInWei, 0, {'from': accounts[1]})
+    return BusStation.deploy(
+        accounts[0], etherInWei, 0, 10 ** 20, {"from": accounts[1]}
+    )
+
+
+@pytest.fixture(scope="module")
+def discountBusStation(BusStation, accounts, chain):
+    etherInWei = 10 ** 18
+    sleep_time = 5 * 60 * 60 * 24
+    chain.sleep(sleep_time)
+    return BusStation.deploy(
+        accounts[0], etherInWei, 0, 10 ** 10, {"from": accounts[1]}
+    )
+
+
+@pytest.fixture(scope="module")
+def departedBus(BusStation, accounts, chain):
+    etherInWei = 10 ** 18
+    sleep_time = 5 * 60 * 60 * 24
+    chain.sleep(sleep_time)
+    deployed = BusStation.deploy(
+        accounts[0], etherInWei, 0, 10 ** 20, {"from": accounts[1]}
+    )
+
+    # arrange
+    riderOneAmount = 10 ** 18 - 1
+    riderTwoAmount = 5
+    startingDestinationBalance = accounts[0].balance()
+    startingRiderOneBalance = accounts[1].balance()
+    startingRiderTwoBalance = accounts[2].balance()
+
+    # act
+    deployed.buyBusTicket({"from": accounts[1], "amount": riderOneAmount})
+    deployed.buyBusTicket({"from": accounts[2], "amount": riderTwoAmount})
+    deployed.triggerBusRide()
+    return deployed
--- tests/test_BusStation_buyBusTicket.py
+++ tests/test_BusStation_buyBusTicket.py
@@ -2,24 +2,45 @@ import brownie
 from brownie.test import given, strategy
 
 
-@given(amount=strategy('uint', max_value=10**19, min_value=1))
+@given(amount=strategy("uint", max_value=10 ** 19, min_value=1))
 def test_buy_bus_ticket_success(accounts, unlockedBusStation, amount):
-    tx = unlockedBusStation.buyBusTicket({'from': accounts[3], 'amount': amount})
+    tx = unlockedBusStation.buyBusTicket({"from": accounts[3], "amount": amount})
     assert unlockedBusStation._ticketTotal() == amount
-    assert tx.events['TicketPurchased'][0]['_from'] == accounts[3]
-    assert tx.events['TicketPurchased'][0]['_value'] == amount
+    assert tx.events["TicketPurchased"][0]["_from"] == accounts[3]
+    assert tx.events["TicketPurchased"][0]["_value"] == amount
+
+
+@given(amount=strategy("uint", min_value=10 ** 10, max_value=10 ** 18))
+def test_cannot_overpay(accounts, discountBusStation, amount):
+    with brownie.reverts("Cannot exceed max ticket value."):
+        tx = discountBusStation.buyBusTicket({"from": accounts[3], "amount": amount})
+
+
+@given(amount=strategy("uint", max_value=10 ** 18, min_value=1))
+def test_can_double_ticket(accounts, unlockedBusStation, amount):
+    tx = unlockedBusStation.buyBusTicket({"from": accounts[3], "amount": amount})
+    tx = unlockedBusStation.buyBusTicket({"from": accounts[3], "amount": amount})
+    assert unlockedBusStation._ticketTotal() == amount * 2
+
+
+def test_second_purchase_cannot_overpay(accounts, discountBusStation):
+    amount = 10 ** 10 - 100000
+    tx = discountBusStation.buyBusTicket({"from": accounts[3], "amount": amount})
+    with brownie.reverts("Cannot exceed max ticket value."):
+        tx = discountBusStation.buyBusTicket({"from": accounts[3], "amount": amount})
+
 
 def test_buy_bus_ticket_no_money_sent(accounts, unlockedBusStation):
-    with brownie.reverts("Need to pay something for the ticket."):
-        unlockedBusStation.buyBusTicket({'from': accounts[3], 'amount': 0})
+    with brownie.reverts("Need to pay more for ticket."):
+        unlockedBusStation.buyBusTicket({"from": accounts[3], "amount": 0})
+
 
 def test_buy_bus_ticket_bus_already_left(accounts, unlockedBusStation):
-    riderOneAmount = 10**18 - 1
+    riderOneAmount = 10 ** 18 - 1
     riderTwoAmount = 5
-    unlockedBusStation.buyBusTicket({'from': accounts[1], 'amount': riderOneAmount})
-    unlockedBusStation.buyBusTicket({'from': accounts[2], 'amount': riderTwoAmount})
+    unlockedBusStation.buyBusTicket({"from": accounts[1], "amount": riderOneAmount})
+    unlockedBusStation.buyBusTicket({"from": accounts[2], "amount": riderTwoAmount})
     unlockedBusStation.triggerBusRide()
 
     with brownie.reverts("The bus already left."):
-        unlockedBusStation.buyBusTicket({'from': accounts[3], 'amount': 0})
-
+        unlockedBusStation.buyBusTicket({"from": accounts[3], "amount": 0})
